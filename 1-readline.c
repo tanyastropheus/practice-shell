898c3188522da0b17983a75266bd408e5df4d8a2
Tanya Kryukova <ttkryukova@gmail.com>
2017-07-09 23:33:53 +0000

    fixed write count to the bytes read by getline()

--- 1-readline.c
+++ 1-readline.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 /**
  * main - display prompt, read command line input, and write input to stdout
  *
@@ -9,7 +10,7 @@
 
 int main(void)
 {
-	ssize_t w_comd;
+	ssize_t w_comd, r_count;
 	size_t size;
 	char *buf;
 
@@ -19,20 +20,25 @@ int main(void)
 
 	if (write(STDOUT_FILENO, "$ ", 2) == -1)
 	{
-		perror("Error: cannot display prompt\n");
+		errno = EIO;
+		perror("write() cannot display prompt");
 		return (-1);
 	}
 
-	if (getline(&buf, &size, stdin) == -1)
+	r_count = getline(&bug, &size, stdin);
+
+	if ((r_count == -1)
 	{
-		perror("Error: cannot read line\n");
+		errno = EIO;
+		perror("getline() cannot read input");
 		return (-1);
 	}
 
-	w_comd = write(STDOUT_FILENO, buf, size);
-	if ((size_t) w_comd != size)
+	w_comd = write(STDOUT_FILENO, buf, r_count);
+	if (w_comd != r_count)
 	{
-		perror("Error: cannot display input\n");
+		errno = EIO;
+		perror("write() cannot display input");
 		free(buf);
 		return (-1);
 	}
