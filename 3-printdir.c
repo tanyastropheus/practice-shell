2ac7da9b2050c2a76e9f044352b337de352bf765
Tanya Kryukova <ttkryukova@gmail.com>
2017-07-13 05:03:20 +0000

    first commit

---
+++ 3-printdir.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * print_dir - prints each directory contained in the the environment
+ * variable PATH, one directory per line.
+ *
+ * Return: 0 if successful
+ */
+
+int print_dir(void)
+{
+	char *dir_s, *dir;
+
+	dir_s = _getenv("PATH");
+	dir = strtok(dir_s, ":");
+
+	while (dir)
+	{
+		if (write(STDOUT_FILENO, dir, _strlen(dir)) == -1)
+			perror("Error");
+		if (write(STDOUT_FILENO, "\n", 1) == -1)
+			perror("Error");
+		dir = strtok(NULL, ":");
+	}
+	return (0);
+}
